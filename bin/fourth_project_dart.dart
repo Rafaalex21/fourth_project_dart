4d72fcdbcd0abdd5ecfec4a590db64d6ee3258fb
Rafaalex21 <rafaalepsantos94@gmail.com>
2022-11-24 17:36:39 -0300

    Initial commit

---
+++ bin/fourth_project_dart.dart
@@ -0,0 +1,36 @@
+import 'dart:io';
+
+void main() {
+  print('Started main');
+  functionOne();
+  print('Finished main');
+}
+
+void functionOne() {
+  print('Started FO1');
+  try {
+    functionTwo();
+  } on FormatException catch (e) {
+    print('foi capturada dentro da FO1');
+    print(e.message);
+    print(e.source);
+
+  }on IOException catch(e){
+    print('foi capturada uma IOException');
+    e.toString();
+  }
+
+  on HttpException catch (e) {
+    e.toString();
+  }
+  print('Finished F01');
+}
+
+void functionTwo() {
+  print('Started F02');
+  for (int i = 1; i <= 5; i++) {
+    print(i);
+    double amount = double.parse('not a nmumber');
+  }
+  print('Finished F02');
+}
